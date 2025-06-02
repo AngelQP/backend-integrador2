106904d48f7959c43ef93c29f853b6be985d17d9
Saul-villar <saul.villar.85@gmail.com>
2025-06-01 23:25:27 -0500

    proveedores back

--- API/Modules/Proveedores/ProveedoresController.cs
+++ API/Modules/Proveedores/ProveedoresController.cs
@@ -11,11 +11,13 @@ namespace Ferreteria.GestionVentas.API.Modules.Proveedores
     {
         private readonly IConfiguration _configuration;
         private readonly IWebHostEnvironment _env;
+        private readonly IGestionVentasModule _proveedores;
 
-        public ProveedoresController(IConfiguration configuration, IWebHostEnvironment env)
+        public ProveedoresController(IConfiguration configuration, IWebHostEnvironment env, IGestionVentasModule proveedores)
         {
             _configuration = configuration;
             _env = env;
+            _proveedores = Proveedores;
         }
 
         [AllowAnonymous]
@@ -24,5 +26,28 @@ namespace Ferreteria.GestionVentas.API.Modules.Proveedores
         {
             return Ok($"Proveedores Service. version: {_configuration["version"]} - Environment: {_env.EnvironmentName}");
         }
-    }
+
+        [AllowAnonymous]
+        [HttpPost("Proveedor")]
+        public async Task<ActionResult> ProveedorCreate(ProveedoresRequest request)
+        {
+            var command = new CrearProveedorComand(
+                request.Nombre,
+                request.Ruc,
+                request.Direccion,
+                request.Telefono,
+                request.Correo,
+                request.FechaRegistro
+            );
+            return Ok(await _proveedores.ExecuteCommandAsync(command));
+        }
+        [HttpGet("Proveedor")]
+        [Produces(typeof(GetProveedorDTO))]
+        public async Task<IActionResult> ProveedorGet([FromQuery] string? nombre, [FromQuery] string? ruc, [FromQuery] int startAt, [FromQuery] int? maxResult, CancellationToken cancellationToken)
+        {
+            var filtro = new GetProveedorFilter(nombre, ruc, startAt, maxResult);
+            var query = new GetProveedorQuery<GetProveedorDTO, GetProveedorFilter>(startAt, maxResult, filtro);
+
+            return Ok(await _proveedores.ExecuteQueryAsync(query));
+        }
 }
---
+++ API/Modules/Proveedores/ProveedoresRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ferreteria.GestionVentas.API.Modules.Proveedores
+{
+	public class ProveedoresRequest
+	{
+		public string Nombre { get; set; }
+		public string Ruc { get; set; }
+		public string Direccion { get; set; }
+		public string Telefono { get; set; }
+		public string Correo { get; set; }
+		public DateTime FechaRegistro { get; set; }
+    }
+}
