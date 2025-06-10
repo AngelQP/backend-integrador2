fc197e2941c86298ec8543f2d36d3c6b892bbe79
Olger huayna <olgerhuaynaguzman@gmail.com>
2025-06-10 04:21:41 -0500

    agregarLote

--- Modules/GestionVentas/Application/Producto/CrearLote/CrearLoteComandHandler.cs
+++ Modules/GestionVentas/Application/Producto/CrearLote/CrearLoteComandHandler.cs
@@ -5,6 +5,7 @@ using Ferreteria.Modules.GestionVentas.Domain.DTO.Producto;
 using Ferreteria.Modules.GestionVentas.Domain.Repository;
 using Ferreteria.Modules.GestionVentas.Domain.Servicios;
 using Ferreteria.Modules.GestionVentas.Domain.Users;
+using NPOI.SS.Formula.Functions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,7 +45,33 @@ namespace Ferreteria.Modules.GestionVentas.Application.Producto.CrearLote
                 FechaCreacion = DateTime.UtcNow,
                 IdProveedor = request.IdProveedor
             });
+            var producto = await _productoRepository.ProductGetById(request.IdProducto);
+            if (producto == null)
+            {
+                return RequestResult.WithError("Producto no encontrado.");
+            }
+            producto.Stock += (int)request.CantidadInicial;
 
+            // 4. Actualizar el producto con el nuevo stock
+            await _productoRepository.ActualizarProducto(new ActualizarProductoRequest
+            {
+                IdProducto = producto.IdProducto,
+                Nombre = producto.Nombre,
+                Descripcion = producto.Descripcion,
+                PrecioUnitario = producto.PrecioUnitario,
+                Stock = producto.Stock,
+                Categoria = producto.Categoria,
+                CodigoBarra = producto.CodigoBarra,
+                UnidadMedida = producto.UnidadMedida,
+                EstadoRegistro = producto.Estado,
+                Sku = producto.Sku,
+                Marca = producto.Marca,
+                Modelo = producto.Modelo,
+                Subcategoria = producto.Subcategoria,
+                ImpuestoTipo = producto.ImpuestoTipo,
+                Costo = producto.Costo,
+                Proveedor = producto.Proveedor,
+            });
             return RequestResult.Success();
         }
     }
